46c4f04f30cda13bf63ecbb89675e21b46abf642
rkcoachingclasses4-tech <rkcoachingclasses4@gmail.com>
2026-06-12 20:15:12 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -84,11 +84,13 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
